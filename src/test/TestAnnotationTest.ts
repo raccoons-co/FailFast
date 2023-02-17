ad9f922a8c9c52b53a97a04bd5400b3f817784c3
Oleksii Kucheruk <50623030+iselo@users.noreply.github.com>
2023-02-17 12:57:23 +0200

    Contribute package to the registry (#4)
    Adds dry run validation of Node.js package packing and test against multiple versions of Node.js with CircleCI.

--- src/test/TestAnnotationTest.ts
+++ src/test/TestAnnotationTest.ts
@@ -2,21 +2,27 @@ import {Test} from "../main/index";
 import {assert} from "chai";
 import TestsInventory from "../main/TestsInventory";
 
-
 export default class TestAnnotationTest {
 
-    public testShouldFail() {
-        console.log("Test");
+    public ignoreMethodAtTheBeginning() {
+        assert.fail("Non test method at the beginning shouldn't fail.");
     }
 
     @Test
     public testsCountAfterFirstTest() {
-        assert.equal(TestsInventory.instance().methods().length, 1,"Incorrect tests count");
+        assert.equal(TestsInventory.instance().methods().length, 1);
+    }
+
+    public ignoreMethodInTheMeddle() {
+        assert.fail("Non test method at the middle shouldn't fail.");
     }
 
     @Test
     public testsCountAfterSecondTest() {
-        assert.equal(TestsInventory.instance().methods().length, 2,"Incorrect tests count");
+        assert.equal(TestsInventory.instance().methods().length, 2);
     }
 
+    public ignoreMethodInTheEnd() {
+        assert.fail("Non test method at the end shouldn't fail.");
+    }
 }
