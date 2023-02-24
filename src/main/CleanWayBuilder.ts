d3e7b24434f0590279a7e54f932f00fccce3ea3c
Oleksii Kucheruk <ok+github@raccoons.co>
2023-02-24 23:06:02 +0200

    Remove `Function` types

--- src/main/CleanWayBuilder.ts
+++ src/main/CleanWayBuilder.ts
@@ -2,17 +2,11 @@ import TestCaseInventory from "./TestCaseInventory";
 
 export default class CleanWayBuilder  {
 
-    private testCaseClasses: Array<Function> = [];
-
-    public use(className: Function) {
-        this.testCaseClasses.push(className);
+    public use(...args: any[]) {
         return this;
     }
 
     public build() {
-        this.testCaseClasses.forEach((className)=> {
-            console.log(className);
-        });
         TestCaseInventory.instance().summary();
     }
 }
