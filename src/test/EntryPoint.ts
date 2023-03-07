30efbd329bae868c8373d97c89215247c15ea30d
Oleksii Kucheruk <ok+github@raccoons.co>
2023-03-07 11:37:16 +0200

    Add `BrainTest`

--- src/test/EntryPoint.ts
+++ src/test/EntryPoint.ts
@@ -2,11 +2,13 @@ import {BrainException, CleanWayBuilder} from "../main/index";
 import {assert} from "chai";
 import YourTest from "./YourTest";
 import FailedTestCaseTest from "./FailedTestCaseTest";
+import BrainTest from "./BrainTest";
 
 assert.throws(() => {
         CleanWayBuilder.instance()
             .assign(new YourTest())
             .assign(new FailedTestCaseTest())
+            .assign(new BrainTest())
             .build();
     },
     BrainException,
