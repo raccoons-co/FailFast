7a278354dab558700dcf0dbd4a3f24e24fc95e89
iselo <ok+github@raccoons.co>
2023-03-15 10:58:35 +0200

    Remove redundant cast

--- src/main/bugeye/eventbus/common/ThrownException.ts
+++ src/main/bugeye/eventbus/common/ThrownException.ts
@@ -18,7 +18,7 @@ export default class ThrownException implements Neuron {
     @Log
     activate(): void {
         Brain.instance()
-            .learn(LogRecord, new LogRecord(this.exception.stack as string));
+            .learn(LogRecord, new LogRecord(this.exception.stack));
         throw new BrainException(this.exception.stack);
     }
 }
