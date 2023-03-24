0db9acb9fc0f524ea7cdec3f7b9f781d674b6a75
iselo <ok+github@raccoons.co>
2023-03-24 09:37:59 +0200

    Fix package docs

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,21 +1,18 @@
 /**
  * © 2023 Raccoons. Developing a simple way to change.
  *
- * The library provides an `EntryPoint` to `@Test`-driven development
- * discipline practice.
- * The library can:
- * - Run `@Test` method;
- * - Make class instances `@Immutable`;
- * - `@Log` method call while debugging.
+ * The library provides an EntryPoint to @Test-driven development discipline practice.
+ *
+ * @Test case annotation will call your class method to test the correct
+ * behaviour/functionality, features of an application.
  *
  * @example
  * ```typescript
- * @Immutable
  * export default class YourTest {
  *
  *     @Test
  *     public nothing() {
- *         // But your assertions here.
+ *         assert.ok("But your assertions here.");
  *     }
  * }
  * ```
