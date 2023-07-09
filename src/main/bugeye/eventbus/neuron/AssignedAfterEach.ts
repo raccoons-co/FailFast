db010bf5b4790c28b2f3582eab05ea028e1b5963
iselo <ok+github@raccoons.co>
2023-07-09 17:48:25 +0300

    Extract base class for `AssignedBeforeEach` and `AssignedAfterEach`

--- src/main/bugeye/eventbus/neuron/AssignedAfterEach.ts
+++ src/main/bugeye/eventbus/neuron/AssignedAfterEach.ts
@@ -1,20 +1,7 @@
-import {Immutable, Strict} from "@raccoons-co/ethics";
-import {Method} from "@raccoons-co/genera";
-import Neuron from "../Neuron";
-import RecognitionPayload from "../RecognitionPayload";
+import {Immutable} from "@raccoons-co/ethics";
+import TestCaseTransition from "./TestCaseTransition";
 
 @Immutable
-export default class AssignedAfterEach implements Neuron {
-
-    private readonly method: Method;
-
-    constructor(method: Method) {
-        this.method = Strict.notNull(method);
-    }
-
-    public activate(testClassInstance: RecognitionPayload) {
-        Strict.notNull(testClassInstance);
-        this.method.call(testClassInstance.valueOf());
-    }
+export default class AssignedAfterEach extends TestCaseTransition {
 }
 
---
+++ src/main/bugeye/eventbus/neuron/TestCaseTransition.ts
@@ -0,0 +1,18 @@
+import {Strict} from "@raccoons-co/ethics";
+import {Method} from "@raccoons-co/genera";
+import Neuron from "../Neuron";
+import RecognitionPayload from "../RecognitionPayload";
+
+export default abstract class TestCaseTransition implements Neuron {
+
+    private readonly method: Method;
+
+    constructor(method: Method) {
+        this.method = Strict.notNull(method);
+    }
+
+    public activate(testClassInstance: RecognitionPayload) {
+        Strict.notNull(testClassInstance);
+        this.method.call(testClassInstance.valueOf());
+    }
+}
